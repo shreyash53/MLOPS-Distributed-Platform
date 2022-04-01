5ddf603edd94ed66bfe886e468bf4529084d9fc4
kamalphoolwani <itsmekamal25@gmail.com>
2022-04-01 23:39:23 +0530

    changed dbconfig.py

--- RequestManager/dbconfig.py
+++ RequestManager/dbconfig.py
@@ -1,8 +1,12 @@
 import mongoengine as db
-
+import os
+import dotenv
+dotenv.load_dotenv()
 
 database_name = 'test'
-DB_URI =  'mongodb+srv://kamal:kamal123@cluster0.lzygp.mongodb.net/{}?retryWrites=true&w=majority'.format(database_name)
+DB_URI = 'mongodb+srv://{}:{}@{}/{}?retryWrites=true&w=majority'.format(
+    os.get.environ('MONGODB_CLUSTER'), os.get.environ('MONGODB_USER'), os.get.environ('MONGODB_PASS'), database_name)
+
 
 def mongodb():
     db.connect(host=DB_URI)
