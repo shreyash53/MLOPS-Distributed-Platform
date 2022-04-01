2f3f02e99ea832d28ab1efc1dbbed475b83b3fad
av-dx <aashwin.vaish@research.iiit.ac.in>
2022-04-01 18:49:37 +0530

    Add shared database URI

--- NotificationManager/dbconfig.py
+++ NotificationManager/dbconfig.py
@@ -2,7 +2,7 @@ import mongoengine as db
 
 
 database_name = 'test'
-DB_URI =  'mongodb://localhost/test'.format(database_name)
+DB_URI =  'mongodb+srv://kamal:kamal123@cluster0.lzygp.mongodb.net/{}?retryWrites=true&w=majority'.format(database_name)
 
 def mongodb():
     db.connect(host=DB_URI)
