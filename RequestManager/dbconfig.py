ecf765755a13f0b13868264cd2af3bd7d49e9840
kamalphoolwani <itsmekamal25@gmail.com>
2022-04-01 17:12:08 +0530

    changed dconfig.py

--- RequestManager/dbconfig.py
+++ RequestManager/dbconfig.py
@@ -1,9 +1,8 @@
-from flask import Flask
 import mongoengine as db
 
-database_name = 'sample'
-password = 'test123'
-DB_URI =  'mongodb+srv://kamal:kamal123@cluster0.lzygp.mongodb.net/firstdb?retryWrites=true&w=majority'
+
+database_name = 'test'
+DB_URI =  'mongodb+srv://kamal:kamal123@cluster0.lzygp.mongodb.net/{}?retryWrites=true&w=majority'.format(database_name)
 
 def mongodb():
     db.connect(host=DB_URI)
