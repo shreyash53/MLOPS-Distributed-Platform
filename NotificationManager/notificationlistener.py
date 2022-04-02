7e8023022267b833d3cb12c001eb97d75095f510
av-dx <aashwin.vaish@research.iiit.ac.in>
2022-04-02 12:32:09 +0530

    Handle JSONDecodeError

--- NotificationManager/notificationlistener.py
+++ NotificationManager/notificationlistener.py
@@ -6,7 +6,8 @@ URL="http://127.0.0.1:8000"
 
 listener = KafkaConsumer('notifications')
 for msg in listener:
-    j = json.loads(msg.value)
-    r = requests.post(URL+'/notify', json={"recipient_id" : j['recipient_id'], "msg" : j['msg']})
-    print(r)
-    print(msg)
+    try:
+        j = json.loads(msg.value)
+        r = requests.post(URL+'/notify', json={"recipient_id" : j['recipient_id'], "msg" : j['msg']})
+    except json.JSONDecodeError as e:
+        print(str(e) + " in " + msg.value.decode('UTF-8'))
