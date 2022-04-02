80d322d345ea9c9be44fd426d73c74ecc0a5b126
kamalphoolwani <itsmekamal25@gmail.com>
2022-04-02 16:10:36 +0530

    changed Validation FIle

--- GatewayComponents/AppUpload/validate.py
+++ GatewayComponents/AppUpload/validate.py
@@ -1,104 +1,123 @@
 import json
 import os.path
 from os import path
+import requests
+from Utilities.models import aimodels
 
-def error(val,error_name):
-    print("Key Error: {} {}!".format(val, error_name))
+URL = 'http://127.0.0.1/sensor_validate'
+
+def error(val, error_name):
+    msg = "Key Error: {} {}!".format(val, error_name)
+    temp = {
+        "msg": msg,
+        "status": 0
+    }
+    print(temp)
+    return temp
+
+
+def succ(msg):
+    temp = {
+        "msg": msg,
+        "status": 1
+    }
+    print(temp)
+    return temp
 
 def key_exist(data,key):
     for i in key:
         if data.get(i) is not None:
-            if(i == "services"):
+            if(i == "sensors"):
                 for j in data[i]:
-                    if service_validate(j) == False:
-                        return False
-            elif(i == "sensors"):
-                    for j in data[i]:
-                        if sensors_validate(j) == False:
-                            return False
+                    sensor_temp = sensors_validate(j)
+                    if sensor_temp['status'] == 0:
+                        return sensor_temp
+                sensor_data = {
+                    "Details": data[i]
+                }
+                res = requests.post(URL,json=sensor_data).content()
+                if(res['status']==0):
+                    return error(res['sensorid'],"Sensor Type Not Present")
             elif(i == "models"):
                 for j in data[i]:
-                    if model_validate(j) == False:
-                        return False
+                    model_temp = model_validate(j)
+                    if model_temp['status'] == 0:
+                        return model_temp
+                    if aimodels.object(modelName=data[i]['modelname']).count()==0:
+                        return error(data[i]['modelname'],"Not Present")
 
         else:   
-            error(i,"Not Found")
-            return False
-    return True
+            return error(i,"Not Found")
+    return succ("All Keys Present")
 
-def key_exist_2(data,key):
+def key_exist_2(data, key):
     for i in key:
         if data.get(i) is not None:
             pass
         else:
-            error(i,"Not Found")
-            return False
-    return True            
+            return error(i, "Not Found")
+    return succ("Found")
 
-def data_exist(data,key):
+
+def data_exist(data, key):
     for i in key:
         if data[i]:
             pass
         else:
-            error(i,"has no Data")
-            return False
-    return True
+            return error(i, "has no Data")
+    return succ("Data Exist")
 
 
 def sensors_validate(data):
-    key = ["sensorname","sensorid","sensortype"]
-    if key_exist_2(data,key):
-        if data_exist(data,key):
-            return True
-        else:
-            return False
+    key = ["sensorname", "sensorid", "sensortype", "sensordatatye"]
+    check_key = key_exist_2(data, key)
+    if check_key['status'] == 1:
+        check_data = data_exist(data, key)
+        # if check_data['status']==1:
+        return check_data
+        # else:
+        #     return check_data
     else:
-        return False
+        return check_key
 
-def service_validate(data):
-    key = ["servicename","serviceloc"]
-    if key_exist_2(data,key):
-        if data_exist(data,key):
-            return True
-        else:
-            return False
-    else:
-        return False
 
 def model_validate(data):
-    key = ["modelname","modelid","modelloc"]
-    if key_exist_2(data,key):
-        if data_exist(data,key):
-            return True
-        else:
-            return False
+    key = ["modelname", "modelid", "modelloc"]
+    check_key = key_exist_2(data, key)
+    if check_key['status'] == 1:
+        check_data = data_exist(data, key)
+        return check_data
+        # if data_exist(data, key):
+        #     return True
+        # else:
+        #     return False
     else:
-        return False
+        return check_key
 
 
 def validate_contract(contract):
-#    contract = 'contract.json'
+    #    contract = 'contract.json'
     contract_file = open(contract)
     contract_data = json.load(contract_file)
 
-
-    key = ["application_name","services","models","sensors"]
-
-    if key_exist(contract_data,key):
-        if data_exist(contract_data,key):
-            print("Good to go!!")
-            return 1
+    key = ["application_name", "models", "sensors"]
+    contract_key = key_exist(contract_data, key)
+    if contract_key['status'] == 1:
+        contract_data =  data_exist(contract_data, key)
+        if contract_data['status']==1:
+            return succ("Good to go!!")
         else:
-            print("Data required in all fields!!")
+            return contract_data
 
     else:
-        print("Upload With Valid contract.json")
+        return contract_key
+        # print("Upload With Valid contract.json")
 
 
-def file_present(p,fileName):
+def file_present(p, fileName):
     p = p/fileName
-    if path.exists(p)==False:
-        error(fileName,"File Not Found")
-        return False
-    else: 
-        return True
+    if path.exists(p) == False:
+        err = error(fileName, "File Not Found")
+        return err
+    else:
+        return succ("File Present")
