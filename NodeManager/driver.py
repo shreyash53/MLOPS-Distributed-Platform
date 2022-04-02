2dfe784e726541e5971b44cf0073e5088f418a17
shreyash53 <shreyash.agrawal@students.iiit.ac.in>
2022-04-02 13:34:01 +0530

    bug fix

--- NodeManager/driver.py
+++ NodeManager/driver.py
@@ -2,4 +2,4 @@ from node_manager import app
 from utilities.constants import static_ip, static_port
 
 if __name__ == '__main__':
-    app.run(debug=True, localhost=static_ip, port=static_port)
+    app.run(debug=True, host=static_ip, port=static_port)
