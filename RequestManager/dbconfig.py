08a6a8eab8ebee45a343dc076f8d882629c07ade
kamalphoolwani <itsmekamal25@gmail.com>
2022-04-02 11:57:02 +0530

    corrected dbconfig.py

--- RequestManager/dbconfig.py
+++ RequestManager/dbconfig.py
@@ -3,9 +3,9 @@ import os
 import dotenv
 dotenv.load_dotenv()
 
-database_name = 'test'
+database_name = 'requestmanager_db'
 DB_URI = 'mongodb+srv://{}:{}@{}/{}?retryWrites=true&w=majority'.format(
-    os.get.environ('MONGODB_CLUSTER'), os.get.environ('MONGODB_USER'), os.get.environ('MONGODB_PASS'), database_name)
+    os.environ.get('MONGODB_USER'), os.environ.get('MONGODB_PASS'), os.environ.get('MONGODB_CLUSTER'),database_name)
 
 
 def mongodb():
