d033b14f24f961626b41bb1125facb286c45afe7
av-dx <aashwin.vaish@research.iiit.ac.in>
2022-04-02 16:51:39 +0530

    Changed string field to DateTime

--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -1,20 +1,14 @@
-from operator import eq
+from time import sleep
 from flask import Flask,request
-from json import loads
 import json
 from mongoengine.document import Document
 from mongoengine.fields import *
-import pymongo
 import threading
 import datetime
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 
-from ias_hacka2.dbconfig import mongodb
-from ias_hacka2.scheduler import get_cur_date_time
-
-
-from Utilities.constants import *
+from dbconfig import *
 from json import dumps
 
 
@@ -25,10 +19,11 @@ class Schedules(db.Document):
     app_instance_id = db.StringField(primary = True)
     app_name = db.StringField(required = True)
     app_id = db.StringField(required=True)
-    starttime = db.StringField(required=True)
+    starttime = db.DateTimeField(required=True)
     repetition = db.IntField(required=True)
     interval= db.StringField(required=True)
-    endtime = db.StringField(required=True)
+    endtime = db.DateTimeField(required=True)
+    duration = db.DateTimeField(required=False)
     sensors = db.StringField(required=True)
 
     def to_json(self):
@@ -50,7 +45,7 @@ def get_app_instance_id():
     initstr=last_id[:4]+str(last_num)
     return initstr
 
-
+Scheduling
 @app.route('/schedule_application',methods=['POST'])
 def scheduleapplication():
     try:
@@ -59,16 +54,20 @@ def scheduleapplication():
         new_schedule = Schedules(app_instance_id = app_instance_id
                                 ,app_name = all_details['app_name']
                                 ,app_id = all_details['app_id']
-                                ,starttime = all_details['starttime']
+                                ,starttime = parsedatetime(all_details['starttime'])
                                 ,repetition = all_details['repetition']
                                 ,interval = json.dumps(all_details['interval'])
-                                ,endtime = all_details['endtime']
+                                ,endtime = parsedatetime(all_details['endtime'])
+                                ,duration = parsedatetime(all_details['endtime']) - parsedatetime(all_details['starttime'])
                                 ,sensors = json.dumps(all_details['sensors']))
 
         new_schedule.save()
     except Exception as e:
         return e
     return "OK"
+
+def parsedatetime(date_time_str):
+    return datetime.strptime(date_time_str, '%d/%m/%y %H:%M:%S')
 
 def get_cur_Time():
     cur_datetime = datetime.datetime.now()
@@ -97,16 +96,25 @@ def send_to_deployment_service(type1, message):
     producer.close()
 
 
+def get_services_bw(starttime, endttime):
+    res = Schedules.objects(kwargs={"$lte" : endttime, "$gt" : starttime})
+    return res
+
 class StartService(threading.Thread):
     def __init__(self): 
         threading.Thread.__init__(self) 
     
     def run(self):
         while(True):
-            today,HH,MM=get_cur_Time()
-            hour = int(HH)
-            minute=int(MM)
-            mycursor=db.Scheduling.find({ 'startdate':today })
+            sleep(60)
+            # TODO : A late request from RM can cause events to be missed
+            
+            now = datetime.datetime.now()
+            slotbegin = now - datetime.timedelta(minutes=1)
+            slotend = now + datetime.timedelta(minutes=1)
+
+            to_start = get_services_bw(slotbegin, slotend)
+
 
             # send_to_deployment_service('start',appid)
 
