1a07f0e0f9ff5ad18465ca9be5743f85690ba074
Shravan Sharma <55808378+shravan-sharma-github@users.noreply.github.com>
2022-04-03 15:08:01 +0530

    modify logging service

--- LogginService/dbconfig.py
+++ LogginService/dbconfig.py
@@ -1,8 +1,14 @@
 import mongoengine as db
-
+import os
+import dotenv
+dotenv.load_dotenv()
 
 database_name = 'logging_db'
-DB_URI =  'mongodb+srv://kamal:kamal123@cluster0.lzygp.mongodb.net/{}?retryWrites=true&w=majority'.format(database_name)
+mg_user = os.getenv('MONGODB_USER')
+mg_pass = os.getenv('MONGODB_PASS') 
+cluster = os.getenv('MONGODB_CLUSTER')
+DB_URI ='mongodb+srv://{}:{}@{}/{}?retryWrites=true&w=majority'.format(mg_user,mg_pass , cluster,database_name)
+
 
 class Logs(db.Document):
     service_name = db.StringField()
