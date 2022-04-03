22469aebc50b234eea8f23e546a61a8917c3c459
AardG <AardG@github.com>
2022-04-03 17:53:58 +0530

    added slcm and monitoring service

---
+++ Deployer/app_model_deploy.py
@@ -0,0 +1,158 @@
+from flask import Flask,request, render_template
+import json
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
+from kafka import KafkaConsumer
+from json import loads
+from dbconfig import *
+import os
+
+class applications(db.Document):
+    appName = db.StringField(required=True,unique=True)
+    path = db.StringField(required=True)
+    contract= db.StringField(required=True)
+
+    def to_json(self):
+        return {
+            "appName": self.appName,
+            "path":self.path,
+            "contract":self.contract
+        }
+
+class aimodels(db.Document):
+    modelName = db.StringField(required=True,unique=True)
+    path = db.StringField(required=True)
+    contract = db.StringField(required=True)
+    def to_json(self):
+        return {
+            "modelName": self.modelName,
+            "path":self.path,
+            "contract":self.contract
+        }
+
+def appdeploy():
+    consumer = KafkaConsumer(
+    'scheduler_service',
+     bootstrap_servers=[os.getenv('kafka_bootstrap')],
+    auto_offset_reset='earliest', 
+     enable_auto_commit=True,
+     group_id='my-group',
+     value_deserializer=lambda x: loads(x.decode('utf-8')))
+    
+    producer = KafkaProducer(bootstrap_servers=[os.getenv('kafka_bootstrap')],
+                        value_serializer=lambda x: 
+                        dumps(x).encode('utf-8'))
+
+    for message in consumer:
+        message = message.value
+        appName=message.get("app_name")
+        # print()
+        # print()
+        # print('{} added to {}'.format(appName, message))
+        app= applications.objects(appName=appName).first()
+        # print()
+        # print()
+        # print(app)
+        # print("b n")
+        if(app==None):
+            continue
+        # print("a n")
+        # print()
+        # print()
+        # print()
+        # print(app.to_json())
+        app=app.to_json()
+        loc=app.get('path')
+        appId=message.get("app_id")
+        contract=app.get('contract')
+        contract=json.loads(contract)
+        # print()
+        # print()
+        # print()
+        # print(contract)
+        appInstanceId=message.get('app_instance_id')
+        app_details={}
+        app_details["appId"]=appId
+        app_details["appName"]=appName
+        app_details["appLoc"]=loc
+        app_details["appInstanceId"]=appInstanceId
+        # print()
+        # print()
+        # print(app_details)
+        models = contract["models"]
+        sensors_list=contract["sensors"]
+        models_list=[]
+        # print()
+        # print()
+        # print(models)
+        # print()
+        # print()
+        # print(sensors_list)
+        for model in models:
+            model_details={}
+            modelId=model.get("modelid")
+            modelname=model.get("modelname")
+            # print(modelId)
+            # print(modelname)
+            model_app= aimodels.objects(modelName=modelname).first()
+            if(model_app==None):
+                continue
+            model_app=model_app.to_json()
+            # print(model_app)
+            model_location=model_app.get("path")
+            model_details["model_Id"]=modelId
+            model_details["model_name"]=modelname
+            model_details["model_location"]=model_location
+            models_list.append(model_details)
+        # print("Model list")
+        # print(models_list)
+        sensors=message.get("sensors")
+        sensors=json.loads(sensors)
+        # print(type(sensors))
+        # print(sensors)
+        sen_lis=[]
+        for each_sensor in sensors_list:
+            sen={}
+            # print()
+            # print("each sensor")
+            # print(each_sensor)
+            # print(type(sensors))
+            # print(sensors)
+            for each_bindings in sensors:
+                # print("each binding")
+                # print(each_bindings)
+                if each_sensor['sensorname']==\
+                    each_bindings['sensor_name']:
+                    sen["sensor_app_id"]=each_sensor['sensorid']
+                    sen["sensor_name"]=each_sensor['sensorname']
+                    sen["sensor_binding_id"]=each_bindings['sensor_binding_id']
+                    sen_lis.append(sen)
+
+        # print()
+        # print()
+        # print("Final_sensor_list")            
+        # print(sen_lis)
+        # data_sensor={
+        #     "sensor_list":sensors
+        # }
+        data_app={
+            "app":app_details,
+            "models":models_list,
+            "request_type":message.get('request_type'),
+            "sensors":sen_lis
+       }
+        # print(data_app)
+        # producer.send('sensor_list', value=data_sensor)
+        # sleep(5)
+        producer.send('app_deploy', value=data_app)
+        sleep(5)
+        print("data sent!!")
+    
+
+
+if __name__ == '__main__':
+    db = mongodb()
+    appdeploy()
+
+    
---
+++ ServiceLifeCycleManager/ServiceLifecycleManager.py
@@ -0,0 +1,103 @@
+from flask import Flask, request
+import service_utilities as sv
+import kafka
+from json import loads
+import threading
+import dotenv
+import os
+dotenv.load_dotenv()
+
+app = Flask(__name__)
+
+PORT = os.getenv("PORT")
+def consume():
+	consumer = kafka.KafkaConsumer(
+    'register',
+    bootstrap_servers=['20.219.107.251:9092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='my-group',
+    value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+	for message in consumer:
+		data = message.value
+		instance = data["instance_id"]
+		if data["request_type"] == "register":
+			data["state"] = "running"
+			sv.savetodb(data)
+		elif data["request_type"] == "unregister":
+			sv.updatedb({"instance_id"  : data["instance_id"]} , {"state" :"stopped" })
+
+
+''''
+# expected request from node manager when docker service is started  
+@app.route("/register", methods=["POST"])
+def service_start():
+	data = request.get_json()
+	if data["type"] == "application":
+		models = data["models"]
+		for x in models:
+			sv.inc_service(x.name , x.type)
+
+	return sv.savetodb(data)
+
+# when a serivce is stopped voluntarily
+@app.route("/service_stop",methods =["POST"])
+def service_stop():
+	data = request.get_json()
+	#need to notify monitoring service
+	return sv.updatedb({"instance_id"  : data["instance_id"]} , {"state" :"killed" })
+'''
+
+#hit by service to get port of another service
+@app.route("/service_lookup", methods=["POST"])
+def service_lookup():
+	request_data = request.get_json()
+	ser=request_data["service_name"]
+	obj = sv.fetchdb({"service_name" :ser })[0]
+	print(obj.to_json(),"dsfsddsfdsfsd")
+	if(obj!=None and obj["state"] == "running"):
+		return {"ip": obj["ip"] ,"port" : obj["port"] , "instance_id" : obj["instance_id"]}
+	else:
+		return "service not found"
+
+#monitor will hit this api when it does not get a heartbeat from a service
+@app.route("/service_dead", methods = ["POST"])
+def dead_service():
+	data = request.json
+	print(data)
+	name = data['instance_id']
+	obj = sv.fetchdb({"instance_id" : name })
+	print(obj[0].state)
+	sv.updatedb({"instance_id" : name }, {"state" : "dead"})
+	
+	# restart
+	# if obj.service_type ==  "platform_service":
+		
+	# else:
+	return "ok"
+
+@app.route("/get_services/<stype>")
+def get_services(stype):
+	if stype == "running":
+		data =sv.fetchdb({"state" : "running"})
+		lst = []
+		for x in data:
+			lst.append([x.service_id,x.service_type,x.service_name])
+		return  lst
+	elif stype == "killed":
+		data = sv.fetchdb({"state" : "scheduled"})
+		lst = []
+		for x in data:
+			lst.append([x.service_id,x.service_type,x.service_name])
+		return  lst
+	else:
+		return "invalid type"
+
+
+
+
+if __name__ == '__main__':
+	t1 = threading.Thread(target =consume)
+	t1.start()
+	app.run(port=PORT, debug=True, )
