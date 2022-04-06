11d15326d1cff5d921a039fac58b50e49bbd33f1
Karan-10 <itsmekamal25@gmail.com>
2022-04-07 03:36:19 +0530

    deleted unrequired files

--- GatewayComponents/Utilities/ModelZip/model1/model.py
+++
@@ -1,9 +0,0 @@
-import random
-class rng:
-  def preprocess(self,a):
-    return (a)
-  def predict(self,arg):
-    random.seed(arg)
-    return random.random()
-  def postprocess(self,a):
-    return a
