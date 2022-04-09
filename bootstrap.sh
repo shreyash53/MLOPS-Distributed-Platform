c72debdf686b563a7bede2637ef9fcdcc9a0014b
Shravan Sharma <55808378+shravan-sharma-github@users.noreply.github.com>
2022-04-09 15:37:29 +0530

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,12 +20,5 @@
 
 python3 -m venv shra
 source ./shra/bin/activate
-pip install -r requirements.txt
-pip install -r ./ServiceLifeCycleManager/requirements.txt
-pip install -r ./ChildNode/requirements.txt
-pip install -r ./Scheduler/requirements.txt
-pip install -r ./SensorManager/requirements.txt
-pip install -r ./Deployer/requirements.txt
-pip install -r ./NodeManager/requirements.txt
 python bootstrap.py
 deactivate
