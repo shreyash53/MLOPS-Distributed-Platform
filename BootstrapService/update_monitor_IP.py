d5e2d5f765b4a02fa882ba2f2f3725960fc5ef92
Shravan Sharma <55808378+shravan-sharma-github@users.noreply.github.com>
2022-04-10 17:39:13 +0530

    debug bootstrapping

--- BootstrapService/update_monitor_IP.py
+++ BootstrapService/update_monitor_IP.py
@@ -2,31 +2,39 @@ import re
 
 TEMPLATE_PATH = "./BootstrapService/Template_Dockerfile.txt"
 
-# def generate_env_string(env_file_path):
-#     try:
-#         env_file = open(env_file_path, 'r')
-#         env_str = env_file.read()
-#         env_file.close()
-#         return env_str
-#     except Exception as e:
-#         return ""
+def generate_env_string(env_file_path):
+    try:
+        env_file = open(env_file_path, 'r')
+        env_lines = env_file.readlines()
+        if len(env_lines) == 0:
+            raise Exception("No lines present in env file")
+        env_str = "ENV "
+        for line in env_lines:
+            env_str = env_str + " \   \n" + line.strip() 
+        env_file.close()
+        return env_str
+    except Exception as e:
+        return ""
 
 def create_docker_file(destination_folder_path, monitor_ip, entry_point_py_file_name):
+    """Creating docker file in root folder"""
 
     docker_file = open(TEMPLATE_PATH, 'r')
     docker_template = docker_file.read()
 
     # erase existing content
-    open(destination_folder_path+"/"+'Dockerfile', 'w').close()
+    open(destination_folder_path+'/Dockerfile', 'w').close()
 
     # open file again and write
-    new_docker_file = open(destination_folder_path+"/"+'Dockerfile', 'w')
+    new_docker_file = open(destination_folder_path+'/Dockerfile', 'w')
 
     docker_template = re.sub(r'<monitor_ip>', monitor_ip, docker_template)
-    # env_str = generate_env_string(destination_folder_path+"/.env")
-    # if env_str == "":
-    #     return "Error: .env file not found in "+destination_folder_path
-    # docker_template = re.sub(r'<env_var>', env_str, docker_template)
+    # docker_template = re.sub(r'<source_folder>', destination_folder_path, docker_template)
+    
+    env_str = generate_env_string(".env")
+    if env_str == "":
+        return "Error: .env file not found in "+destination_folder_path
+    docker_template = re.sub(r'<env_vars>', env_str, docker_template)
     docker_template = re.sub(
         r'<entry_point>', entry_point_py_file_name, docker_template)
     new_docker_file.write(docker_template)
