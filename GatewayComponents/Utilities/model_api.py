467a5e7b6d8bf0cb942da85f06fed5f5bdc95fab
Karan-10 <itsmekamal25@gmail.com>
2022-04-14 01:18:36 +0530

    code changes for adding in req.txt

--- GatewayComponents/Utilities/model_api.py
+++ GatewayComponents/Utilities/model_api.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify, request
-import sys
-from util.constants import *
+
 import os
 import dotenv
 dotenv.load_dotenv() 
 PORT = os.environ.get("SERVICE_PORT")
 # Assuming : Model Class file is included in "dependencies"
+
 <other_dependencies>
+import <fileName>
 app = Flask(__name__)
 
 
@@ -14,16 +15,7 @@ app = Flask(__name__)
 def get_result():
     mdict = request.json()
     res = request.get_json()
-    return jsonify({"result":model.'<predict_fun_name>'(**res)})
-    # array = pd.DataFrame(mdict)
-
-    # pickle_file = open('<pickle_file_path>', 'rb')
-    # model = pickle.load(pickle_file)
-    # res = model.<preprocess_fun_name>(<preprocessing_para_name>)
-    # res = model.<predict_fun_name>(<predict_para_name>)
-    # res = model.<postprocess_fun_name>(<postprocessing_para_name>)
-    # pickle_file.close()
-    # return str(res)
+    return jsonify({"result":<fileName>.<predict_fun_name>(**res)})
 
 if __name__ == "__main__":
     app.run(debug=False, port=PORT, host='0.0.0.0')
