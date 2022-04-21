30f70a23bb25aadd0de791b92d766c8ace370ab8
av-dx <aashwin.vaish@research.iiit.ac.in>
2022-04-21 22:03:47 +0530

    Unattended installation

--- setuphostvm.sh
+++ setuphostvm.sh
@@ -3,6 +3,7 @@
 apt update
 apt --yes --force-yes install docker
 apt --yes --force-yes install docker-compose
+apt --yes --force-yes install python3.8-venv
 
 mkdir -p /lib/docker/cli-plugins
 cd /lib/docker/cli-plugins
@@ -11,4 +12,6 @@ mv buildx-v0.8.2.linux-amd64 docker-buildx
 chmod +x docker-buildx
 
 usermod -aG docker azureuser
+timedatectl set-timezone Asia/Kolkata
+
 echo "Restart your VM now....."
