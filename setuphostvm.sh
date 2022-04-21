be08429b634681593cb4ce035832a7188aa007fe
Ubuntu <azureuser@iasvm-alpha.rzc22y3dhx1uffs2bposfo5lsc.tx.internal.cloudapp.net>
2022-04-21 13:38:29 +0000

    VM Setup Script

---
+++ setuphostvm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt update
+apt install docker
+apt install docker-compose
+
+mkdir -p /lib/docker/cli-plugins
+cd /lib/docker/cli-plugins
+wget https://github.com/docker/buildx/releases/download/v0.8.2/buildx-v0.8.2.linux-amd64
+mv buildx-v0.8.2.linux-amd64 docker-buildx
+chmod +x docker-buildx
+
+usermod -aG docker azureuser
+echo "Restart your VM now....."
