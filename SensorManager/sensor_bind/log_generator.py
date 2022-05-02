a789df0c3d22ceac8cbe8b3cb0de1e7832eee4b6
Pulkit112 <79832220+Pulkit112@users.noreply.github.com>
2022-05-02 10:56:50 +0530

    Bug Fixes

--- SensorManager/sensor_bind/log_generator.py
+++ SensorManager/sensor_bind/log_generator.py
@@ -2,27 +2,23 @@ import os
 from dotenv import load_dotenv
 import json
 from kafka import KafkaProducer
-from Scheduler.utilities.constant import SERVICE_NAME
-
-
+# from Scheduler.utilities.constant import SERVICE_NAME
 load_dotenv()
 log_topic = os.getenv("KAFKA_LOG_TOPIC")
 kafka_server = os.getenv("kafka_bootstrap")
-SERVICE_NAME = os.getenv("sensor_manager_service_name") 
-# TODO: change service name env variable 
+SERVICE_NAME = os.getenv("sensor_manager_service_name")
+# TODO: change service name env variable
 # logs = KafkaProducer(bootstrap_servers=kafka_server,value_serializer=lambda x:dumps(x).encode('utf-8'))
 
 
-
-def send_log(log_type, msg ):
-    logs = KafkaProducer(bootstrap_servers=kafka_server, value_serializer=lambda x:json.dumps(x).encode('utf-8'))
+def send_log(log_type, msg):
+    logs = KafkaProducer(bootstrap_servers=kafka_server,
+                         value_serializer=lambda x: json.dumps(x).encode('utf-8'))
     possible_log_type = ['INFO', 'WARN', 'ERR']
     if log_type not in possible_log_type:
-        raise Exception('ERR: log_type provided is wrong\n It should be INFO, WARN or ERR')
+        raise Exception(
+            'ERR: log_type provided is wrong\n It should be INFO, WARN or ERR')
 
-    data = {"type" : log_type , "service_name" : SERVICE_NAME,"msg" : msg}
+    data = {"type": log_type, "service_name": SERVICE_NAME, "msg": msg}
     logs.send(log_topic, data)
     return 1
-
-		
-
